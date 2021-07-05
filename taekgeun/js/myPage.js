c655585240afcf2bafc85943de002dc8126fe18f
taek <vvision009@gmail.com>
2021-07-05 09:32:52 +0900

    회원정보 보기, 비밀번호 페이지

---
+++ taekgeun/js/myPage.js
@@ -0,0 +1,27 @@
+window.addEventListener("load",function(){
+    
+    //회원정보 초기화
+    const memInfo = {
+        
+    };
+
+
+    //변경버튼
+    const changeButton = document.querySelector("#change");
+
+    //아이디
+    document.querySelector("#id").value = "SWIFT09";
+    document.querySelector("#pw").value="123456";
+
+    const id = document.querySelector("#id").value;
+    const pw = document.querySelector("#pw").value;
+
+
+    changeButton.onclick = function(){
+        var url = "verify-password.html?";
+        url = url.concat('id=',id);
+        url = url.concat('&pw=',pw);
+        location.href=url;
+        
+    }
+})
